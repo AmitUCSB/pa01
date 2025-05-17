1770a15638812e9d35b044e87e663ee5d74ffeac
Amit Karthikeyan <amitk@169-231-34-198.wireless.ucsb.edu>
2025-05-16 21:46:32 -0700

    fixed makefile

--- card.h
+++ card.h
@@ -5,4 +5,26 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <string>
+#include <iostream>
+
+class Card {
+public:
+    Card(char suit, char value);
+
+    char getSuit() const;
+    char getVal() const;
+    std::string toString() const;
+
+    bool operator<(const Card& other) const;
+    bool operator>(const Card& other) const;
+    bool operator==(const Card& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Card& c);
+
+private:
+    char suit; 
+    char val;  
+};
+
 #endif
--- card_list.h
+++ card_list.h
@@ -1,8 +1,62 @@
 // card_list.h
-// Author: Your name
+// Author: Amit Karthikeyan, Neil Gadagkar
 // All class declarations related to defining a BST that represents a player's hand
-
 #ifndef CARD_LIST_H
 #define CARD_LIST_H
 
+
+#include "card.h"
+
+class card_list {
+    public:
+        card_list();
+        ~card_list();
+        void insert(const Card& c);
+        void remove(const Card& c);
+        bool contains(const Card& c) const;
+        void printDeck() const;
+        class Iterator;
+        Iterator begin() const;
+        Iterator end() const;
+        Iterator rbegin() const;
+        Iterator rend() const;
+    
+    private:
+        struct Node {
+            Card info;
+            Node* left;
+            Node* right;
+            Node* parent;
+            Node(const Card& c) : info(c), left(nullptr), right(nullptr), parent(nullptr) {}
+    };
+        Node* root;
+    
+        Node* insert(Node* node, const Card& c, Node* parent);
+        Node* find(Node* node, const Card& c) const;
+        Node* getMin(Node* node) const;
+        Node* getMax(Node* node) const;
+        Node* remove(Node* node, const Card& c);
+        void clear(Node* node);
+    };
+
+    class card_list::Iterator {
+        public:
+            Iterator();
+            Iterator(Node* ptr);
+            const Card& operator*() const;
+            const Card* operator->() const;
+            Iterator& operator++();  
+            Iterator& operator--(); 
+            bool operator==(const Iterator& other) const;
+            bool operator!=(const Iterator& other) const;
+        
+        private:
+            Node* current;
+            Node* getSuccessorNode(Node* node) const;
+            Node* getPredecessorNode(Node* node) const;
+        };
+
+
+void playGame(card_list& alice, card_list& bob);
+        
 #endif
